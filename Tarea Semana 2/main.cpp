9e3be03d8f2ca7ced473e43dfc41e3eeb4a13043
User <as.vargasa@uea.edu.ec>
2025-06-04 17:44:14 -0500

    kkk

---
+++ Tarea Semana 2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: User
+ *
+ * Created on 4 de junio de 2025, 17:43
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
