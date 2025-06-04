39e14ca6d158935b48e70ba3aedd5d1429cfc908
User <as.vargasa@uea.edu.ec>
2025-06-04 17:38:57 -0500

    kkk

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: User
+ *
+ * Created on 4 de junio de 2025, 17:37
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+
+    printf("Hola");
+    return 0;
+}
+
